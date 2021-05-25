964c86aa2f3b6d3b64a4a807ea946b1f3cb9ab4a
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-05-26 01:31:23 +0530

    `ClassPoolManager` also includes the project files and jars

--- dagger-track/src/main/java/com/droidsingh/daggertrack/ClassPoolManager.kt
+++ dagger-track/src/main/java/com/droidsingh/daggertrack/ClassPoolManager.kt
@@ -3,12 +3,17 @@ package com.droidsingh.daggertrack
 import com.android.build.api.transform.TransformInvocation
 import javassist.ClassPool
 
-internal class ClassPoolManager(private val classPool:ClassPool) {
+internal class ClassPoolManager(private val classPool: ClassPool) {
 
     fun buildProjectClassPool(transformInvocation: TransformInvocation): ClassPool {
         // External Deps
         transformInvocation.referencedInputs.forEach { transformInput ->
             transformInput.directoryInputs.forEach { classPool.insertClassPath(it.file.absolutePath) }
+            transformInput.jarInputs.forEach { classPool.insertClassPath(it.file.absolutePath) }
+        }
+        // Project files
+        transformInvocation.inputs.forEach { transformInput ->
+            transformInput.directoryInputs.forEach{ classPool.insertClassPath(it.file.absolutePath) }
             transformInput.jarInputs.forEach { classPool.insertClassPath(it.file.absolutePath) }
         }
         return classPool
--- dagger-track/src/test/kotlin/com/droidsingh/daggertrack/ClassPoolManagerTest.kt
+++ dagger-track/src/test/kotlin/com/droidsingh/daggertrack/ClassPoolManagerTest.kt
@@ -13,22 +13,22 @@ import java.io.File
 
 internal class ClassPoolManagerTest {
 
+    private val transformInvocation = mock<TransformInvocation>()
+    private val transformInput = mock<TransformInput>()
+    private val directoryInput = mock<DirectoryInput>()
+    private val jarInput = mock<JarInput>()
+    private val jarFile = mock<File>()
+    private val directoryInputFile = mock<File>()
+    private val classPool = mock<ClassPool>()
+    private val transformInputCollection = arrayListOf(transformInput)
+    private val directoryInputCollection = arrayListOf(directoryInput)
+    private val jarInputCollection = arrayListOf(jarInput)
+
     @Test
     fun `it builds class pool of external libraries`() {
         // given
-        val transformInvocation = mock<TransformInvocation>()
-        val transformInput = mock<TransformInput>()
-        val directoryInput = mock<DirectoryInput>()
-        val jarInput = mock<JarInput>()
-        val jarFile = mock<File>()
-        val directoryInputFile = mock<File>()
-        val classPool = mock<ClassPool>()
-        val transformInputCollection = arrayListOf(transformInput)
-        val directoryInputCollection = arrayListOf(directoryInput)
-        val jarInputCollection = arrayListOf(jarInput)
         val externalJarPath = "/jars/myExternalJar.jar"
         val externalClassDirectoryPath = "/directory/MyExternalClass"
-
         whenever(jarFile.absolutePath).thenReturn(externalJarPath)
         whenever(directoryInputFile.absolutePath).thenReturn(externalClassDirectoryPath)
         whenever(directoryInput.file).thenReturn(directoryInputFile)
@@ -45,5 +45,28 @@ internal class ClassPoolManagerTest {
         // then
         verify(classPool).insertClassPath(externalJarPath)
         verify(classPool).insertClassPath(externalClassDirectoryPath)
+    }
+
+    @Test
+    fun `it includes project files inputs in class pool`() {
+        // given
+        val jarPath = "/jars/myInternalJar.jar"
+        val directoryPath = "/directory/MyApplicationClass"
+        whenever(jarFile.absolutePath).thenReturn(jarPath)
+        whenever(directoryInputFile.absolutePath).thenReturn(directoryPath)
+        whenever(directoryInput.file).thenReturn(directoryInputFile)
+        whenever(jarInput.file).thenReturn(jarFile)
+        whenever(transformInvocation.inputs).thenReturn(transformInputCollection)
+        whenever(transformInput.directoryInputs).thenReturn(directoryInputCollection)
+        whenever(transformInput.jarInputs).thenReturn(jarInputCollection)
+
+        // when
+        ClassPoolManager(classPool).buildProjectClassPool(
+            transformInvocation
+        )
+
+        // then
+        verify(classPool).insertClassPath(jarPath)
+        verify(classPool).insertClassPath(directoryPath)
     }
 }
