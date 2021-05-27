1c5e1aa69854db94765323e0eb158e0ccc88a6d6
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-05-27 23:54:57 +0530

    Adds functionality to calculate CPU time in `dagger-track-clocks`
    - Calculates CPU time of main thread by default

--- dagger-track-clocks/src/main/java/com/droidsingh/daggertrack/DaggerTrackClocks.kt
+++ dagger-track-clocks/src/main/java/com/droidsingh/daggertrack/DaggerTrackClocks.kt
@@ -4,7 +4,19 @@ import android.os.SystemClock
 
 object DaggerTrackClocks {
 
+    init {
+        System.loadLibrary("cpu_time")
+    }
+
+    @JvmStatic
     fun getUptimeMillis(): Long {
         return SystemClock.uptimeMillis()
     }
+
+    @JvmStatic
+    fun getCpuTimeMillis(): Long {
+        return getCpuTime()
+    }
+
+    private external fun getCpuTime(): Long
 }
