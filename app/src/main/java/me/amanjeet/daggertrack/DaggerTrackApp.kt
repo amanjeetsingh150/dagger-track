883b9862ccacc09eb0ad8945e7bfc6a257d76cf8
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-06-12 03:02:13 +0530

    Integrates hilt in dagger-track sample app

--- app/src/main/java/me/amanjeet/daggertrack/DaggerTrackApp.kt
+++ app/src/main/java/me/amanjeet/daggertrack/DaggerTrackApp.kt
@@ -4,22 +4,8 @@ import android.app.Application
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
-import me.amanjeet.daggertrack.di.DaggerApplicationComponent
+import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
-class DaggerTrackApp : Application(), HasAndroidInjector {
-
-    @Inject
-    internal lateinit var androidInjector: DispatchingAndroidInjector<Any>
-
-    override fun onCreate() {
-        super.onCreate()
-        DaggerApplicationComponent
-            .builder()
-            .bindApplication(this)
-            .build()
-            .inject(this)
-    }
-
-    override fun androidInjector(): AndroidInjector<Any> = androidInjector
-}
+@HiltAndroidApp
+open class DaggerTrackApp : Application()
