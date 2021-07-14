d081e09c9bcdebfc6c7f208ed8064d8d9123d6c7
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-07-14 23:00:46 +0530

    Refactors `ClassPoolFactoryTest` to assert if external classes are in classpool

--- dagger-track/src/test/kotlin/me/amanjeet/daggertrack/ClassPoolFactoryTest.kt
+++ dagger-track/src/test/kotlin/me/amanjeet/daggertrack/ClassPoolFactoryTest.kt
@@ -1,12 +1,9 @@
 package me.amanjeet.daggertrack
 
-import com.android.build.api.transform.DirectoryInput
-import com.android.build.api.transform.JarInput
-import com.android.build.api.transform.TransformInput
-import com.android.build.api.transform.TransformInvocation
+import com.android.SdkConstants.DOT_CLASS
 import com.android.build.gradle.BaseExtension
+import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import javassist.ClassPool
 import org.junit.Before
@@ -15,50 +12,35 @@ import java.io.File
 
 internal class ClassPoolFactoryTest {
 
-    private val transformInvocation = mock<TransformInvocation>()
-    private val transformInput = mock<TransformInput>()
-    private val directoryInput = mock<DirectoryInput>()
-    private val jarInput = mock<JarInput>()
-    private val jarFile = mock<File>()
-    private val directoryInputFile = mock<File>()
+    private val defaultClassPool = ClassPool.getDefault()
     private val sdkDirectory = mock<File>()
-    private val classPool = mock<ClassPool>()
     private val android = mock<BaseExtension>()
-    private val transformInputCollection = arrayListOf(transformInput)
-    private val directoryInputCollection = arrayListOf(directoryInput)
-    private val jarInputCollection = arrayListOf(jarInput)
 
     @Before
     fun setup() {
-        whenever(sdkDirectory.absolutePath).thenReturn("/directory/androidSdk")
+        whenever(sdkDirectory.absolutePath).thenReturn("./src/test/resources/fake-android-sdk")
         whenever(android.sdkDirectory).thenReturn(sdkDirectory)
-        whenever(android.compileSdkVersion).thenReturn("28")
+        whenever(android.compileSdkVersion).thenReturn("android-28")
     }
 
     @Test
     fun `it builds class pool of external libraries`() {
         // given
-        val externalJarPath = "/jars/myExternalJar.jar"
-        val externalClassDirectoryPath = "/directory/MyExternalClass"
-        whenever(jarFile.absolutePath).thenReturn(externalJarPath)
-        whenever(directoryInputFile.absolutePath).thenReturn(externalClassDirectoryPath)
-        whenever(directoryInput.file).thenReturn(directoryInputFile)
-        whenever(jarInput.file).thenReturn(jarFile)
-        whenever(transformInvocation.referencedInputs).thenReturn(transformInputCollection)
-        whenever(transformInput.directoryInputs).thenReturn(directoryInputCollection)
-        whenever(transformInput.jarInputs).thenReturn(jarInputCollection)
-        val androidJarPath = "${android.sdkDirectory.absolutePath}/platforms" +
-                "/${android.compileSdkVersion}/android.jar"
+        val classFileInputSequence = File("./src/test/resources")
+            .walkTopDown()
+            .map { it }
+            .filter { it.absolutePath.endsWith(DOT_CLASS) }
 
         // when
-        ClassPoolFactory(classPool).buildProjectClassPool(
-            transformInvocation,
+        val classPool = ClassPoolFactory(defaultClassPool).buildProjectClassPool(
+            classFileInputSequence.toList(),
             android
         )
 
-        // then
-        verify(classPool).insertClassPath(androidJarPath)
-        verify(classPool).insertClassPath(externalJarPath)
-        verify(classPool).insertClassPath(externalClassDirectoryPath)
+        // then, class file from dummy android jar & external class file exist in classpool
+        val classFromJarFile = classPool.get("me.amanjeet.daggertrack.ClassPoolFactory")
+        val applicationComponent = classPool.get("me.amanjeet.daggertrack.di.components.ApplicationComponent")
+        assertThat(classFromJarFile.simpleName).isEqualTo("ClassPoolFactory")
+        assertThat(applicationComponent.simpleName).isEqualTo("ApplicationComponent")
     }
 }
