2887a39ec8ea89b2145e77c4928ba6cbfde71c56
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-07-26 03:22:38 +0530

    Refactors `CtClassTransformations` test to filter dagger subcomponents
    - Adds `MinimalProjectCreator` to create minimal dagger android project

--- dagger-track/src/test/kotlin/me/amanjeet/daggertrack/CtClassTransformationsTest.kt
+++ dagger-track/src/test/kotlin/me/amanjeet/daggertrack/CtClassTransformationsTest.kt
@@ -2,7 +2,6 @@ package me.amanjeet.daggertrack
 
 import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
 import javassist.ClassPool
 import javassist.CtClass
 import me.amanjeet.daggertrack.utils.DaggerComponentsFixtureCreator
@@ -15,7 +14,7 @@ import me.amanjeet.daggertrack.utils.DaggerComponentsFixtureCreator.Companion.VI
 import me.amanjeet.daggertrack.utils.DaggerComponentsFixtureCreator.Companion.VIEW_C_IMPL
 import me.amanjeet.daggertrack.utils.DaggerComponentsFixtureCreator.Companion.VIEW_WITH_FRAGMENT_C_IMPL
 import me.amanjeet.daggertrack.utils.GradleTestRunner
-import me.amanjeet.daggertrack.utils.addSubcomponentAnnotation
+import me.amanjeet.daggertrack.utils.MinimalProjectCreator
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -36,96 +35,21 @@ internal class CtClassTransformationsTest {
     @Before
     fun setUp() {
         gradleTestRunner = GradleTestRunner(testProjectDir)
-        gradleTestRunner.addSrc(
-            srcPath = "minimal/MyApp.java",
-            srcContent =
-            """
-                    package minimal;
-                    import android.app.Application;
-
-                    public class MyApp extends Application {
-                        @Override
-                        public void onCreate() {
-                            super.onCreate();
-                        }
-                    }
-            """.trimIndent()
-        )
-        gradleTestRunner.setAppClassName(".MyApp")
-        gradleTestRunner.addSrc(
-            srcPath = "minimal/ApplicationComponent.java",
-            srcContent = """
-                    package minimal;
-    
-                    import android.app.Application;
-                    import dagger.BindsInstance;
-                    import dagger.Component;
-                    import dagger.android.AndroidInjectionModule;
-                    import dagger.android.support.AndroidSupportInjectionModule;
-                    import minimal.MyApp;
-                    import javax.inject.Singleton;
-    
-                    @Singleton
-                    @Component(
-                        modules = {
-                            AndroidInjectionModule.class,
-                            AppModule.class,
-                            AndroidSupportInjectionModule.class
-                        }
-                    )
-                    interface ApplicationComponent {
-    
-                        void inject(MyApp myApplication);
-    
-                        @Component.Builder
-                        interface Builder {
-                            Builder bindApplication(@BindsInstance Application application);
-                            ApplicationComponent build();
-                        }
-                    }
-                """.trimIndent()
-        )
     }
 
     @Test
     fun `it filters the dagger components`() {
         // given
-        gradleTestRunner.addSrc(
-            srcPath = "minimal/AppModule.java",
-            srcContent = """
-                package minimal;
-
-                import android.app.Application;
-                import android.content.Context;
-                import dagger.Binds;
-                import dagger.Module;
-                import javax.inject.Singleton;
-
-                @Module
-                abstract class AppModule {
-
-                    @Binds
-                    @Singleton
-                    abstract Context provideApplicationContext(Application application);
-                }
-            """.trimIndent()
-        )
-        gradleTestRunner.addDependencies(
-            "implementation 'androidx.appcompat:appcompat:1.1.0'",
-            "implementation 'com.google.dagger:dagger-android-support:2.35.1'",
-            "annotationProcessor 'com.google.dagger:dagger-android-processor:2.35.1'",
-            "annotationProcessor 'com.google.dagger:dagger-compiler:2.35.1'"
-        )
+        MinimalProjectCreator.createDaggerAndroidProject(gradleTestRunner)
+        classPool.makeClass("dagger.android.HasAndroidInjector")
         val result = gradleTestRunner.build(shouldIntegrateDaggerTrack = false)
-        val appClassFile = result.getClassFile("minimal/MyApp.class")
         val appModuleClassFile = result.getClassFile("minimal/AppModule.class")
         val appComponentClassFile = result.getClassFile("minimal/ApplicationComponent.class")
         val daggerAppComponentClassFile = result.getClassFile("minimal/DaggerApplicationComponent.class")
-        val appClass = classPool.makeClass(appClassFile.inputStream())
         val appModule = classPool.makeClass(appModuleClassFile.inputStream())
         val appComponent = classPool.makeClass(appComponentClassFile.inputStream())
         val daggerAppComponent = classPool.makeClass(daggerAppComponentClassFile.inputStream())
-        val ctClassList = listOf(appClass, appComponent, appModule, daggerAppComponent)
+        val ctClassList = listOf(appComponent, appModule, daggerAppComponent)
 
         // when
         val daggerComponents = ctClassList.filterDaggerComponents()
@@ -136,34 +60,21 @@ internal class CtClassTransformationsTest {
     }
 
     @Test
-    fun `it filters out all the subcomponents of component`() {
+    fun `it filters out the subcomponents from components`() {
         // given
-        val homeActivitySubcomponentImpl = classPool.makeClass(
-            "me.amanjeet.daggertrack.di.components.HomeActivitySubcomponentImpl"
-        )
-        val homeFragmentSubcomponentImpl = classPool.makeClass(
-            "me.amanjeet.daggertrack.di.components.HomeFragmentSubcomponentImpl"
-        )
-        // for convenience lets assume subcomponent class is static
-        // dagger does not create static subcomponents
-        // javassist does not supports to create non-static classes
-        val homeFragmentNestedSubcomponentImpl = homeActivitySubcomponentImpl.makeNestedClass(
-            homeFragmentSubcomponentImpl.name,
-            true
-        )
-        prepareHomeActivity(homeActivitySubcomponentImpl)
-        prepareHomeFragment(homeFragmentNestedSubcomponentImpl)
-        val applicationComponent = mock<CtClass>()
-        whenever(applicationComponent.nestedClasses).thenReturn(arrayOf(homeActivitySubcomponentImpl))
+        MinimalProjectCreator.createDaggerAndroidProject(gradleTestRunner)
+        val result = gradleTestRunner.build(shouldIntegrateDaggerTrack = false)
+        val daggerAppComponentClassFile = result.getClassFile("minimal/DaggerApplicationComponent.class")
+        val classFilesDirectory = daggerAppComponentClassFile.parentFile.parentFile
+        val daggerAppComponent = classPool.makeClass(daggerAppComponentClassFile.inputStream())
+        classPool.insertClassPath(classFilesDirectory.absolutePath)
 
         // when
-        val subComponentCtClassList = applicationComponent.filterSubcomponents()
+        val filteredSubComponents = daggerAppComponent.filterSubcomponents()
 
         // then
-        val subcomponentListNames = subComponentCtClassList.map { it.name }
-        assertThat(subcomponentListNames).containsExactly(
-            homeActivitySubcomponentImpl.name, homeFragmentNestedSubcomponentImpl.name
-        )
+        val subComponentNames = filteredSubComponents.map { it.name }
+        assertThat(subComponentNames).containsExactly("minimal.DaggerApplicationComponent\$HomeActivitySubcomponentImpl")
     }
 
     @Test
@@ -194,21 +105,5 @@ internal class CtClassTransformationsTest {
             VIEW_WITH_FRAGMENT_C_IMPL,
             FRAGMENT_C_IMPL
         )
-    }
-
-    private fun prepareHomeActivity(homeActivitySubcomponentImpl: CtClass) {
-        val homeActivitySubcomponent = classPool.makeInterface(
-            "me.amanjeet.daggertrack.di.components.HomeActivitySubcomponent"
-        )
-        homeActivitySubcomponentImpl.addInterface(homeActivitySubcomponent)
-        homeActivitySubcomponent.addSubcomponentAnnotation()
-    }
-
-    private fun prepareHomeFragment(homeFragmentNestedSubcomponentImpl: CtClass) {
-        val homeFragmentSubcomponent = classPool.makeInterface(
-            "me.amanjeet.daggertrack.di.components.HomeFragmentSubcomponent"
-        )
-        homeFragmentNestedSubcomponentImpl.addInterface(homeFragmentSubcomponent)
-        homeFragmentSubcomponent.addSubcomponentAnnotation()
     }
 }
